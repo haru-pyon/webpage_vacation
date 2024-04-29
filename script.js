35677a3da7ffb7c359c774c910e742d2d8345608
haru-pyon <76803431+haru-pyon@users.noreply.github.com>
2024-04-29 20:52:58 +0900

    Initial commit
    add README, index.html, style.css, and script.js

---
+++ script.js
@@ -0,0 +1,8 @@
+function showImage() {
+          event.preventDefault();
+          document.getElementById("imagePopup").style.display = "block";
+        }
+        
+        function hideImage() {
+          document.getElementById("imagePopup").style.display = "none";
+        }
